9d2dfd8ad2013d6ca5b53416678ce4ea4f86dce3
Davi Almeida <lik3games@gmail.com>
2023-07-05 15:47:55 -0300

    +

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
 import Link from 'next/link'
+import { GetStaticProps } from 'next';
 
-const IndexPage = () => (
+const IndexPage = (props) => (
   <>
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">About Plus +</Link>
+      <h2>Data: props.date</h2>
     </p>
   </>
 )
+
+export async function getStaticProps() {
+  const staticDate = new Date()
+  const staticDateString = staticDate.toDateString();
+
+  return {
+    props: {
+      date: staticDateString
+    }
+  }
+
+}
 
 export default IndexPage;
