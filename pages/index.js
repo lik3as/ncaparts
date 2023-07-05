df14da06961add4fc54e64c5540a2ea772812423
Davi Almeida <lik3games@gmail.com>
2023-07-05 10:54:21 -0300

    first page

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import react from 'react'
+import { useState } from 'react';
+
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <Contador />
+    </>
+  );
+}
+
+function Contador() {
+  const [contador,setContador] = useState(1);
+
+
+  function adicionarContador() {
+    setContador(contador + 1);
+  } 
+
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={adicionarContador}>Adicionar</button>
+    </div>
+  )
+}
+
+export default Home;
