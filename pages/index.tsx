11e8714d2bde17a55f57bc5a93d8b636493a5a1c
Davi Almeida <lik3games@gmail.com>
2023-07-07 10:14:34 -0300

    export home as default

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,11 @@
-import Link from 'next/link'
+import { FC } from 'react'
+
+const Home: FC = () => (
+  <>
+    <nav>Navigator</nav>
+    <main>Main</main>
+    <footer>Footer</footer>
+  </>
+);
+
+export default Home;
