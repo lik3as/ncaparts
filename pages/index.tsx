52fc818303bd8e30b36defd7e5c9e67fe6d044c8
lik3as <lik3games@gmail.com>
2023-07-07 09:31:58 -0300

    props iplicit any

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,1 @@
 import Link from 'next/link'
-
-const IndexPage = (props) => (
-  <>
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About Plus +</Link>
-      <h2>Data: {props.date}</h2>
-    </p>
-  </>
-)
-
-export default IndexPage;
