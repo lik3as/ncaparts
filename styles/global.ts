f4547d53e034ada6dce4feb39cdb3a2ef3495156
lik3as <lik3games@gmail.com>
2023-07-10 11:30:37 -0300

    fonts

---
+++ styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export default createGlobalStyle`
+ * {
+  padding: 0;
+  margin: 0;
+ }
+
+ html {
+  background-color: #141414;
+ }
+ 
+ :root {
+  --header-height: 90px;
+  --nav-width: 240px;
+  --color-green: rgba(37,211,102,255);
+
+ }
+
+ @font-face {
+  font-family: 'Dic Sans';
+  src: url('/fonts/dic-sans/Dic Sans Bold.otf');
+  font-weight: 'bold';
+ }
+`
