398059b4c3d6c2c549af08ff62983090f65cb7ed
lik3as <lik3games@gmail.com>
2023-07-10 11:30:14 -0300

    added components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import { FC } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Header from '../components/header';
+import Navigator from '../components/navigator'
+import Global from '../styles/global';
 
 const Home: FC = () => (
   <>
-    <nav>Navigator</nav>
-    <main>Main</main>
+    <Global />
+
+    <header>
+      <Header />
+    </header>
+
+    <main>
+      <Navigator />
+
+    </main>
+
     <footer>Footer</footer>
   </>
 );
