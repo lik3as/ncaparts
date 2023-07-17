da415590535a401b140cc1f93aa1e023625c0cd9
lik3as <lik3games@gmail.com>
2023-07-17 09:25:58 -0300

    products component

---
+++ components/products/index.tsx
@@ -0,0 +1,12 @@
+'use server'
+
+import { FC } from 'react'
+import { Wrapper, Text } from './styles'
+
+const Products: FC = () => {
+  return (
+    <Wrapper>
+      <Text color='white'>Melhores Produtos</Text>
+    </Wrapper>
+  )
+}
---
+++ components/products/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+import ItemWrapper from '../helpers/ItemWrapper'
+import ItemText from '../helpers/ItemText'
+
+const Wrapper = styled(ItemWrapper)`
+  display: ${(props) => props.$display ?? 'flex'};
+  flex-direction: ${(props) => props.$flexDirection ?? 'row'};
+  align-items: ${(props) => props.$alignItems ?? 'center'};
+  justify-content: ${(props) => props.$justifyContent ?? 'center'};
+  margin: ${(props) => props.$margin ?? '20px 0px'};
+  max-width: ${(props) => props.$maxWidth ?? '100%'};
+  min-width: ${(props) => props.$minWidth ?? 'auto'};
+  background-color: ${(props) => props.$backgroundColor ?? 'transparent'};
+`;
+
+const Text = styled(ItemText)`
+  font-family: ${(props) => props.fontFamily ?? 'Arial, Helvetica, sans-serif'};
+  font-weight: ${(props) => props.fontWeight ?? 'normal'};
+  font-size: ${(props) => props.fontSize ?? '1rem'};
+  text-align: ${(props) => props.$textAlign ?? 'center'};
+  color: ${(props) => props.color ?? 'black'};
+`;
+
+export { Wrapper, Text }
