15ec37f4e4873107ebc0162f288d428cffbd5552
lik3as <daviacosta2006@gmail.com>
2023-08-10 14:55:19 -0300

    logo in place of undefined

--- components/sales/list/item/foreground/index.tsx
+++ components/sales/list/item/foreground/index.tsx
@@ -3,14 +3,14 @@ import { FC } from 'react'
 import { Image, Wrapper } from './styles';
 
 interface Props {
-  url: string;
+  url?: string;
   alt: string;
 }
 
-const Foreground: FC<Props> = ({url, alt}) => {
+const Foreground: FC<Props> = ({url, alt}) => { 
   return (
     <Wrapper className='foreground-container' $backgroundColor='#464646'>
-      <Image src={url} alt={alt} width={800} height={800}/>
+      <Image src={(url) ?? '/images/logos/logo-black.png'} alt={alt} width={800} height={800}/>
     </Wrapper>
   )
 }
