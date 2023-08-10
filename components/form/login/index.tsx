4e0c1fadb1d7862a3b36034c575bf9b8954d36f3
lik3as <daviacosta2006@gmail.com>
2023-08-10 13:31:00 -0300

    removed x padding

--- components/form/login/index.tsx
+++ components/form/login/index.tsx
@@ -1,27 +1,25 @@
 import { type FC } from 'react'
 import { FaUser, FaLock } from 'react-icons/fa'
-import { InputGroup, Row, Form, Button } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 
-import { Text, Col, FormControl } from '../styles';
+import { Text } from '../styles';
 import FormItem from '../group';
 
 const LoginForm: FC = () => {
   return (
-      <Form className='px-2 px-sm-0'>
+      <Form className='w-100'>
 
-        <FormItem InputIcon={FaUser} controlId='loginFormEmail' className='mb-2 ps-5 ps-sm-0'
+        <FormItem InputIcon={FaUser} controlId='loginFormEmail' className='mb-2'
         inputPHolder='Digite o seu e-mail'
         inputType='email'
         labelText='Email'
         />
 
-        <FormItem InputIcon={FaLock} controlId='loginFormPasswd' className='mb-3 ps-5 ps-sm-0'
+        <FormItem InputIcon={FaLock} controlId='loginFormPasswd' className='mb-3'
         inputPHolder='Digite a sua senha'
         inputType='password'
         labelText='Senha'
         />
-
-
 
         <Button className='mb-1 w-100 me-5 mx-sm-0'>
           Entrar
