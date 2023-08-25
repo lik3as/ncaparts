97f1aa6a738ae6cfe06ea9a9ee4a9e45bd64ebf9
lik3as <daviacosta2006@gmail.com>
2023-08-25 14:39:51 -0300

    condition if imgSrc is defined

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -11,16 +11,18 @@ const Image: FC<Props> = ({ product }) => {
   let imgSrc = product.imagens[0];
   const logo = '/images/logos/logo-white.png';
 
-  try {
-    axios.get(imgSrc).then((response) => {
-      if (response) imgSrc = imgSrc; /**Do nothing .*/
-      else throw new Error("Response is undefined");
-    })
+  if (imgSrc) {
+    try {
+      axios.get(imgSrc).then((response) => {
+        if (response) imgSrc = imgSrc; /**Do nothing .*/
+        else console.log("Response is undefined");
+      });
 
-  } catch (e) {
-    console.log("URL Without image: " + imgSrc)
-    imgSrc = logo;
-  }
+    } catch (e) {
+      console.log("URL Without image: " + imgSrc)
+      imgSrc = logo;
+    }
+  } else imgSrc = logo;
 
   return <ItemImage src={imgSrc} alt={product.desc || product.nome + ' | ' + product.sku}/>
 }
