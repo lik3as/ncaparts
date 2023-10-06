a7ed0954404b8124c1446beded9b944bcead0c03
lik3as <daviacosta2006@gmail.com>
2023-10-06 14:59:58 -0300

    chore: exporting logo images and empty box image

--- constants/index.ts
+++ constants/index.ts
@@ -7,3 +7,8 @@ dotenv.config({
 })
 
 export const API_URL = (process.env.ENV == 'production') ? process.env.API_URL! : "http://localhost:8080/" ;
+export const logoHorizontalWhite = "/images/logos/logo-h-white.png";
+export const logoVerticalWhite = "/images/logos/logo-white.png";
+export const logoHorizontalBlack = "/images/logos/logo-h-black.png";
+export const logoVerticalBlack = "/images/logos/logo-black.png";
+export const emptyBox = "/images/mpt-box.png";
