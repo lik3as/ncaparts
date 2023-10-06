7d74ef1e4f5c41e2d1caa73b78fc6be8a4686015
lik3as <daviacosta2006@gmail.com>
2023-10-06 15:05:30 -0300

    fix: items will never be null or undefined. The type assertion should be done as soon as possible

--- components/sales/list/index.tsx
+++ components/sales/list/index.tsx
@@ -8,17 +8,16 @@ import IItem from '../../../types/item'
 import Link from "next/link";
 
 interface Props {
-  items: IItem[] | null;
+  items: IItem[];
 }
 
 const List: FC<Props> = ({items}) => { 
-
   return (
     <UL>
-      {items?.map((item, index) => (
-        <Link href={`/${item.produto.tipo?.nome}/${item.produto.sku}`} key={index}>
+      {items.map((item, index) => (
+        <Link href={`/produtos/${item.produto.sku}`} key={index}>
           <ListItem key={index} className="rounded-outline" >
-              <Item cat={item.produto.tipo?.nome as string} name={item.produto?.nome} price={item.valor_real}
+              <Item cat={item.produto.tipos[0].nome} name={item.produto?.nome} price={item.valor_real}
               sku={item.produto.sku} url={item.produto.imagens[0]} desc={item.produto.desc}/>
           </ListItem>
         </Link>
