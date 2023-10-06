6406e23cc7d50732f26fe97b400b8be587840dd9
lik3as <daviacosta2006@gmail.com>
2023-10-06 15:20:25 -0300

    not found catch all route

---
+++ app/[...not_found]/page.tsx
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import Global from "../../styles/global";
+import GlobalThemes from "../../styles/themes";
+import Content from "../../components/layouts/content";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Text } from "../../components/selling/styles";
+import { Image, Wrapper } from "../../components/landing/styles";
+import { API_URL } from "../../constants";
+
+export default async function Page() {
+  const items = await (await fetch(API_URL + "Mercadorias", { cache: "no-store"})).json(); 
+  const tipos = await (await fetch(API_URL + "Categorias/Tipos")).json();
+
+  return(
+    <StrictMode>
+      <Global />
+      <GlobalThemes />
+      
+      <Content tipos={tipos} mainProps={{
+        $overflowY: "hidden",
+        $width: "100vw"
+      }}>
+        <Wrapper $display="flex" $flexDirection="column" $alignItems="center" $justifyContent="space-between">
+          <Text h1 fontSize="4rem"> 404 </Text>
+          <Text h1> Não foi possível encontrar a esta página no momento! </Text>
+          <Image src='/images/logos/logo-white.png'/>
+        </Wrapper>
+      </Content>
+    </StrictMode>
+  )
+}
