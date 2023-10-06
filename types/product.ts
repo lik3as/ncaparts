d940c3653c0805506105c76cd00bebcf8809c3ef
lik3as <daviacosta2006@gmail.com>
2023-10-06 14:52:58 -0300

    fix: add Fabricante field in product type

--- types/product.ts
+++ types/product.ts
@@ -1,16 +1,16 @@
 import ICategoria from "./categoria";
+import Fabricante from "./fabricante";
 
 export default interface IProduct {
-  id: number,
-  nome: string,
-  sku: string,
-  final: boolean,
-  desc: string,
-  imagens: string[],
-  produto: IProduct | null,
-  tipo: ICategoria | null,
-  subtipo: ICategoria | null,
-  marca: ICategoria | null,
-  modelo: ICategoria | null,
-  versao: ICategoria | null,
+  id: number;
+  nome: string;
+  sku: string;
+  final: boolean;
+  desc: string;
+  imagens: string[];
+  produto: IProduct | null;
+  fabricante: Fabricante;
+  tipos: ICategoria[];
+  grupos: ICategoria[];
+  modelos: ICategoria[];
 }
