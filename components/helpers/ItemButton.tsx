1172dec0ebc11d8c73b80997ba269d01d54472e2
lik3as <daviacosta2006@gmail.com>
2023-10-23 15:23:52 -0300

    fix: target prop added

--- components/helpers/ItemButton.tsx
+++ components/helpers/ItemButton.tsx
@@ -1,19 +1,15 @@
 import { FC, ReactNode } from "react";
 
-interface Props {
-  variant?: "secondary" | "primary";
-  className?: string;
-  href?: string;
-  children?: ReactNode;
-  type?: "submit" | "button" | "reset";
-}
+type Props = ButtonProps & AnchorProps;
 
 interface ButtonProps {
   variant?: "secondary" | "primary";
   className?: string;
   children?: ReactNode;
   type?: "submit" | "button" | "reset";
+  target?: "_blank" | "_parent" | "_self" | "_top";
 }
+
 
 interface AnchorProps {
   variant?: "secondary" | "primary";
@@ -21,20 +17,22 @@ interface AnchorProps {
   href?: string;
   children?: ReactNode;
   type?: string;
+  target?: "_blank" | "_parent" | "_self" | "_top";
 }
+
 
 const ButtonElement: FC<ButtonProps> = ({variant, className, children, type}) => {
   return <button className={`${className} btn btn-${variant}`} type={type}>{children}</button>
 }
 
-const AnchorElement: FC<AnchorProps> = ({variant, className, href, children, type}) => {
-  return <a className={`${className} btn btn-${variant}`} role="button" href={href} type={type}>{children}</a>
+const AnchorElement: FC<AnchorProps> = ({variant, className, href, children, type, target}) => {
+  return <a target={target} className={`${className} btn btn-${variant}`} role="button" href={href} type={type}>{children}</a>
 }
 
-const Button: FC<Props> = ({variant, href, children, className, type}) => {
+const Button: FC<Props> = ({variant, href, children, className, type, target}) => {
   if (href) {
     return (
-      <AnchorElement className={className} variant={variant} href={href} type={type}>{children}</AnchorElement>
+      <AnchorElement target={target} className={className} variant={variant} href={href} type={type}>{children}</AnchorElement>
     )
   }
   return (
