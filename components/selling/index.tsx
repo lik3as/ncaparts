1ea832107e221bfaab1cc432d2660310b97cb775
lik3as <daviacosta2006@gmail.com>
2023-10-23 15:26:31 -0300

    fix: limited width in product overview

--- components/selling/index.tsx
+++ components/selling/index.tsx
@@ -16,7 +16,7 @@ const Selling: FC<Props> = ({item}) => {
       <Wrapper $maxWidth='20rem'>
         <Image src={(item.produto.imagens[0]) ?? '/images/logos/logo-white.png'} alt={item.produto.desc + ' | ' + item.produto.sku}/>
       </Wrapper>
-      <Wrapper $margin='0 20px' $flexDirection='column'>
+      <Wrapper $margin='0 20px' $flexDirection='column' className='w-25'>
         <Text h1>{item.produto.nome}</Text>
         <Text fontSize='1.5rem' fontWeight='200'>{item.produto.desc}</Text>
         <Text fontSize='1.5rem' fontWeight='200'>{price}</Text>
