84d12eec18e9a39ba1e5447fd377675cf65fdb51
lik3as <daviacosta2006@gmail.com>
2023-10-27 15:53:19 -0300

    Revert "chore: removed image wrapper and using new image props (sizing)"
    This reverts commit 9329cc9712c455e36dfadb71e6417fbf06dd6fba.

--- components/selling/index.tsx
+++ components/selling/index.tsx
@@ -14,7 +14,9 @@ const Selling: FC<Props> = ({item}) => {
 
   return (
     <Wrapper className="selling-container" $minWidth='100vw' $flexDirection='row'>
-        <Image product={item.produto} width={320} height={320} className='object-fit-contain'/>
+      <Wrapper $maxWidth='20rem'>
+        <Image product={item.produto}/>
+      </Wrapper>
       <Wrapper $margin='0 20px' $flexDirection='column' className='w-50'>
         <Text h1>{item.produto.nome}</Text>
         <Text fontSize='1.5rem' fontWeight='200'>{item.produto.desc}</Text>
